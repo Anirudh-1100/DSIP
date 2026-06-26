4bcdb29328977e897229ee93d016e702af89358f
mohanish-kv <mohanish.kv@acuvate.com>
2026-06-26 16:36:55 +0530

    uploading Program.cs,shipment.cs, ShipmentService.cs

--- DelhiveryConsoleApp/Program.cs
+++ DelhiveryConsoleApp/Program.cs
@@ -1,3 +1,4 @@
+using DelhiveryConsoleApp;
 using System;
 using System.Collections.Generic;
 using System.Data;
---
+++ DelhiveryConsoleApp/Shipment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelhiveryConsoleApp
+{
+    public class Shipment
+    {
+        public int ShipmentId { get; set; }
+        public string AWBNumber { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiverName { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public double WeightKg { get; set; }
+        public string Status { get; set; }
+        public DateTime BookedAt { get; set; }
+    }
+}
